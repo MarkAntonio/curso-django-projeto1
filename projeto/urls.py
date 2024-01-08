a6936f5c42e94f5c8cf1c7a98877bc80be892446
Marco Antonio <marcosdesouza772@gmail.com>
2024-01-08 12:09:30 -0300

    "Developing"

---
+++ projeto/urls.py
@@ -0,0 +1,26 @@
+"""
+URL configuration for projeto project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+# a funçõa include seve para incluí Urls paterns dos apps aqui
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    # aqui a route '' indica que as views de recipe estarão na home do meu site "site.com/"
+    path('', include('recipes.urls'))
+]
